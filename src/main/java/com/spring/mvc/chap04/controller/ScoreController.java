1f1756c202968302f4e701eef2c19a3dab368e3f
oh young seok <oyg9731@naver.com>
2023-11-25 11:10:08 +0900

    # 9. 3 tier architecture

--- src/main/java/com/spring/mvc/chap04/controller/ScoreController.java
+++ src/main/java/com/spring/mvc/chap04/controller/ScoreController.java
@@ -5,18 +5,18 @@ package com.spring.mvc.chap04.controller;
     - 클라이언트의 요청을 받아서 처리하고 응답을 제공하는 객체
 
     # 요청 URL Endpoint
-    1.  학생의 성적정보 등록 폼 화면을 보여주고
-        동시에 지금까지 저장되어있는 성적정보 목록을 조회
+    1. 학생의 성적정보 등록폼 화면을 보여주고
+       동시에 지금까지 저장되어 있는 성적 정보 목록을 조회
     - /score/list   :   GET
 
-    2.  학생의 입력된 성적정보를 데이터베이스에 저장하는 요청
-    - /score/register   :   POST
+    2. 학생의 입력된 성적정보를 데이터베이스에 저장하는 요청
+    - /score/register   :  POST
 
-    3.  성적정보를 삭제하는 요청
-    - /score/remove     :   GET or POST
+    3. 성적정보를 삭제 요청
+    - /score/remove    :  GET or POST
 
-    4.  성적정보 상세 요청
-    - /score/detail     : GET
+    4. 성적정보 상세 조회 요청
+    - /score/detail  :   GET
  */
 
 import com.spring.mvc.chap04.dto.ScoreRequestDTO;
@@ -24,6 +24,7 @@ import com.spring.mvc.chap04.dto.ScoreResponseDTO;
 import com.spring.mvc.chap04.entity.Score;
 import com.spring.mvc.chap04.repository.ScoreRepository;
 import com.spring.mvc.chap04.repository.ScoreRepositoryImpl;
+import com.spring.mvc.chap04.service.ScoreService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,149 +40,105 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/score")
 @RequiredArgsConstructor // final이 붙은 필드를 초기화하는 생성자를 생성
-// @AllArgsConstructor // 모든 필드를 초기화하는 생성자를 생성
+//@AllArgsConstructor // 모든 필드를 초기화하는 생성자를 생성
 public class ScoreController {
 
-    // 저장소에 의존되어 데이터 처리를 위임한다.
-    private final ScoreRepository repository;
+    // 저장소에 의존하여 데이터 처리를 위임한다.
+    // 의존객체는 불변성을 가지는 것이 좋다.
+    private final ScoreService service;
 
     // @Autowired // 스프링에 등록된 빈을 자동주입
     // 생성자 주입을 사용하고 생성자가 단 하나 -> autowired 생략가능
-//    public ScoreController(ScoreRepository repository){
+//    public ScoreController(ScoreRepository repository) {
 //        this.repository = repository;
 //    }
 
-    // 1. 성적 폼 띄우기 + 목록조회
-    // - jsp파일로 입력 폼 화면을 띄워줘야 함 (view 포워딩)
+    //1. 성적 입력폼 띄우기 + 목록조회
+    // - jsp파일로 입력폼 화면을 띄워줘야 함 (view 포워딩)
     // - 저장된 성적정보 리스트를 jsp에 보내줘야 함 (model에 데이터 전송)
-    // - 저장된 성적정보 리스트를 어떻게 가져오느냐 from DB
-
+    // - 저장된 성적정보 리스트를 어떻게 가져오느냐 from 데이터베이스
     @GetMapping("/list")
     public String list(Model model,
                        @RequestParam(defaultValue = "num") String sort) {
-        System.out.println("/score/list GET");
+        System.out.println("/score/list GET !!");
 
-        // db에서 조회한 모든데이터
-        List<Score> scoreList = repository.findAll(sort);
-//        System.out.println(scoreList);
-
-        // 클라이언트가 필요한 일부데이터
-//        List<ScoreResponseDTO> dtoList = new ArrayList<>();
-//        for (Score score : scoreList){
-//            dtoList.add(new ScoreResponseDTO(score);
-//        }
-
-        List<ScoreResponseDTO> dtoList = scoreList.stream()
-                        .map(ScoreResponseDTO::new)
-                        .collect(Collectors.toList());
-
+        List<ScoreResponseDTO> dtoList = service.getList(sort);
         model.addAttribute("sList", dtoList);
 
         return "chap04/score-list";
     }
 
-    // 2. 데이터베이스 저장 요청
+    //2. 성적정보를 데이터베이스에 저장하는 요청
     @PostMapping("/register")
     public String register(ScoreRequestDTO score) {
-        System.out.println("/score/Register POST");
+        System.out.println("/score/register POST !!");
         System.out.println("score = " + score);
 
-        // DTO를 엔터티로 변환 -> 데이터 생성
-        Score savedScore = new Score(score);
-        repository.save(savedScore);
+        service.insertScore(score);
 
         /*
-            forward vs redirect
+            # forward vs redirect
             - 포워드는 요청 리소스를 그대로 전달해줌.
             - 따라서 URL이 변경되지 않고 한번의요청과 한번의 응답만 이뤄짐
 
             - 리다이렉트는 요청후에 자동응답이 나가고
               2번째 자동요청이 들어오면서 2번째 응답을 내보냄
-            - 딸서 2번째 요청의 URL로 자동 변경됨
+            - 따라서 2번째 요청의 URL로 자동 변경됨
          */
+
         // forward할때는 포워딩할 파일의 경로를 적는것
         // ex) /WEB-INF/views/chap04/score-list.jsp
 
-        //redirect할때는 리다이엑트 요청 URL을 적는것
+        // redirect할때는 리다이렉트 요청 URL을 적는것
         // ex) http://localhost:8080/score/detail
+
         return "redirect:/score/list";
     }
 
-    // 3. 삭제요청
+    //3. 성적 삭제 요청
     @RequestMapping(value = "/remove/{stuNum}", method = {RequestMethod.GET, RequestMethod.POST})
     public String remove(HttpServletRequest request,
                          @PathVariable int stuNum) {
-        System.out.printf("/score/remove %s!! \n", request);
-        System.out.println("삭제할 학번 : " + stuNum);
+        System.out.printf("/score/remove %s !!\n", request.getMethod());
+        System.out.println("삭제할 학번: " + stuNum);
 
-        repository.delete(stuNum);
+        service.deleteScore(stuNum);
 
         return "redirect:/score/list";
     }
 
-    // 4. 상세정보 요청
-    // 4. 상세정보 요청
+    //4. 성적 상세 조회 요청
     @GetMapping("/detail")
     public String detail(int stuNum, Model model) {
-        System.out.println("/score/detail GET");
+        System.out.println("/score/detail GET !!");
 
-        Score score = repository.findOne(stuNum);
-        model.addAttribute("s", score);
+        retrieve(stuNum, model);
+
         return "chap04/score-detail";
+    }
+
+    private void retrieve(int stuNum, Model model) {
+        Score score = service.retrieve(stuNum);
+        model.addAttribute("s", score);
     }
 
     // 5. 수정 입력 폼을 열어주는 요청
     // /score/modify : GET
     @GetMapping("/modify")
-    public String modify(int stuNum, Model model){
-        System.out.println(" /score/modify GET!!");
-
+    public String modify(int stuNum, Model model) {
+        System.out.println("/score/modify GET !!");
         retrieve(stuNum, model);
-
         return "chap04/score-modify";
-    }
-
-    private void retrieve(int stuNum, Model model) {
-        Score score = repository.findOne(stuNum);
-        model.addAttribute("s", score);
     }
 
     // 6. 수정 처리 요청
     // /score/modify : POST
     @PostMapping("/modify")
-    public String modify(int stuNum, ScoreRequestDTO dto){
-        System.out.println(" /score/modify POST!!");
-        // 수정의 흐름
-        // 클라이언트가 수정할 데이터를 보냄
-        // -> 서버에 저장되어 있는 기존데이터를 조회해서 수정한다
-        Score score = repository.findOne(stuNum);
-        score.changeScore(dto);
+    public String modify(int stuNum, ScoreRequestDTO dto) {
+        System.out.println("/score/modify POST !!");
+        service.updateScore(stuNum, dto);
+
         return "redirect:/score/detail?stuNum=" + stuNum;
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
