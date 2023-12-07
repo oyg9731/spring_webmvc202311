74207933f5529df9a91cfaab17edd90e9c5ad731
oh young seok <oyg9731@naver.com>
2023-12-07 11:30:31 +0900

    # 12. Spring JDBC

--- src/main/java/com/spring/mvc/chap04/service/ScoreService.java
+++ src/main/java/com/spring/mvc/chap04/service/ScoreService.java
@@ -1,27 +1,28 @@
 package com.spring.mvc.chap04.service;
-
 import com.spring.mvc.chap04.dto.ScoreRequestDTO;
 import com.spring.mvc.chap04.dto.ScoreResponseDTO;
 import com.spring.mvc.chap04.entity.Score;
 import com.spring.mvc.chap04.repository.ScoreRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 /**
  * 컨트롤러와  레파지토리에 사이에 위치하여
  * 중간 로직을 처리하는 역할
  *
  * 컨트롤러 -> 서비스 -> 레파지토리
  */
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Service
 public class ScoreService {
 
     private final ScoreRepository repository;
+
+    public ScoreService(@Qualifier("dbRepo") ScoreRepository repository) {
+        this.repository = repository;
+    }
 
     // 목록 조회 중간처리
     /*
@@ -37,23 +38,19 @@ public class ScoreService {
                 .map(ScoreResponseDTO::new)
                 .collect(Collectors.toList());
     }
-
     // 성적 입력 중간 처리
     public boolean insertScore(ScoreRequestDTO dto) {
         return repository.save(new Score(dto));
     }
-
     // 삭제 중간 처리
     public boolean deleteScore(int stuNum) {
         return repository.delete(stuNum);
     }
-
     // 개별 상세조회 중간 처리
     public Score retrieve(int stuNum) {
         // 데이터 가공 ex) 글제목 줄임처리, 조회수상승 반복방지처리...
         return repository.findOne(stuNum);
     }
-
     // 수정 완료 중간처리
     public void updateScore(int stuNum, ScoreRequestDTO dto) {
         // 수정의 흐름
@@ -62,5 +59,4 @@ public class ScoreService {
         Score score = repository.findOne(stuNum);
         score.changeScore(dto);
     }
-
 }
