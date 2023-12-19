9127df8a1a7125c5eeff901cdfa61e58db286016
oh young seok <oyg9731@naver.com>
2023-12-19 10:51:46 +0900

    # 21. REST API 댓글 목록조회 클라이언트 처리

--- src/main/java/com/spring/mvc/chap05/service/BoardService.java
+++ src/main/java/com/spring/mvc/chap05/service/BoardService.java
@@ -1,9 +1,9 @@
 package com.spring.mvc.chap05.service;
 
 import com.spring.mvc.chap05.common.Search;
-import com.spring.mvc.chap05.dto.BoardDetailResponseDTO;
-import com.spring.mvc.chap05.dto.BoardListResponseDTO;
-import com.spring.mvc.chap05.dto.BoardWriteRequestDTO;
+import com.spring.mvc.chap05.dto.response.BoardDetailResponseDTO;
+import com.spring.mvc.chap05.dto.response.BoardListResponseDTO;
+import com.spring.mvc.chap05.dto.request.BoardWriteRequestDTO;
 import com.spring.mvc.chap05.entity.Board;
 import com.spring.mvc.chap05.repository.BoardMapper;
 import lombok.RequiredArgsConstructor;
--- src/main/java/com/spring/mvc/chap05/service/ReplyService.java
+++ src/main/java/com/spring/mvc/chap05/service/ReplyService.java
@@ -1,23 +1,39 @@
 package com.spring.mvc.chap05.service;
 
-import com.spring.mvc.chap05.entity.Reply;
+import com.spring.mvc.chap05.common.Page;
+import com.spring.mvc.chap05.dto.response.ReplyDetailResponseDTO;
+import com.spring.mvc.chap05.dto.response.ReplyListResponseDTO;
 import com.spring.mvc.chap05.repository.ReplyMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReplyService {
 
     private final ReplyMapper replyMapper;
 
+
     // 댓글 목록 조회
-    public List<Reply> getList(long boardNo){
+    public ReplyListResponseDTO getList(long boardNo, Page page) {
 
-        List<Reply> replyList = replyMapper.findAll(boardNo);
+        // DB에서 댓글 정보 조회
+        List<ReplyDetailResponseDTO> replies = replyMapper.findAll(boardNo, page)
+                .stream()
+                .map(ReplyDetailResponseDTO::new)
+                .collect(Collectors.toList());
 
-        return replyList;
+        // DB에서 총 댓글 수 조회
+        int count = replyMapper.count(boardNo);
+
+        return ReplyListResponseDTO.builder()
+                .replies(replies)
+                .count(count)
+                .build();
     }
 }
