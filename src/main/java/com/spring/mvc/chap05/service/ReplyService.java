471a1390b7e009e85b7fde21003b34fa2bfee4c2
oh young seok <oyg9731@naver.com>
2023-12-20 11:34:38 +0900

    # 23. REST API 댓글 등록 처리, 입력값 검증

--- src/main/java/com/spring/mvc/chap05/service/ReplyService.java
+++ src/main/java/com/spring/mvc/chap05/service/ReplyService.java
@@ -2,6 +2,7 @@ package com.spring.mvc.chap05.service;
 
 import com.spring.mvc.chap05.common.Page;
 import com.spring.mvc.chap05.common.PageMaker;
+import com.spring.mvc.chap05.dto.request.ReplyPostRequestDTO;
 import com.spring.mvc.chap05.dto.response.ReplyDetailResponseDTO;
 import com.spring.mvc.chap05.dto.response.ReplyListResponseDTO;
 import com.spring.mvc.chap05.repository.ReplyMapper;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,5 +39,20 @@ public class ReplyService {
                 .count(count)
                 .pageInfo(new PageMaker(page, count))
                 .build();
+    }
+
+    // 댓글 등록 서비스
+    public ReplyListResponseDTO register(ReplyPostRequestDTO dto) throws SQLException {
+        log.debug("register services execute!!!");
+
+        boolean flag = replyMapper.save(dto.toEntitiy());
+
+        if (!flag){
+            log.warn("reply register failed");
+            throw new SQLException("댓글 저장 실패!!!");
+        }
+
+        // 등록이 성공하면 새롭게 갱신된 1페이지 댓글내용을 재 조회해서 응답한다
+        return getList(dto.getBno(), new Page(1, 5));
     }
 }
